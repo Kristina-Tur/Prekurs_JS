ee11e0e0aa0f26147fa406809c57b96fbd58c775
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-02-28 19:18:04 +0300

    data

---
+++ independent-work/lesson1/playlist/index.js
@@ -0,0 +1,28 @@
+const playlist = {
+  cover: {
+    title: 'Beyonce – Diva',
+    albumСover: '.\assets\images\ae5dabea04cba4ce60fa41dfaf36eec_large.png'
+  },
+  tracksList: [
+    {
+      title: 'Beyonce – Brak My Soul',
+      trackCover: '.\assets\images\ae5dabea04cba4ce60fa41dfaf36eec_large.png',
+      song: '.\assets\tracks\beyonce_ - _diva_(muztune.me).mp3'
+    },
+    {
+      title: 'Beyonce – Spirit',
+      trackCover: '.\assets\images\ae5dabea04cba4ce60fa41dfaf36eec_large.png',
+      song: '.\assets\tracks\beyonce_ - _diva_(muztune.me).mp3'
+    },
+    {
+      title: 'Beyonce – Formation',
+      trackCover: '.\assets\images\ae5dabea04cba4ce60fa41dfaf36eec_large.png',
+      song: '.\assets\tracks\beyonce_ - _diva_(muztune.me).mp3'
+    },
+    {
+      title: "Beyonce – Don't hurt yourself",
+      trackCover: '.\assets\images\ae5dabea04cba4ce60fa41dfaf36eec_large.png',
+      song: '.\assets\tracks\beyonce_ - _diva_(muztune.me).mp3'
+    }
+  ]
+}
