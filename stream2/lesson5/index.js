a77b100ca5670d4eb29dbd91a0a0d8196facc89b
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-03-04 15:06:37 +0300

    a

--- stream2/lesson5/index.js
+++ stream2/lesson5/index.js
@@ -1,19 +1,18 @@
-import { Game } from "./ui/game/game.component.js";
-import { data, subscribe } from "./data.js";
-import { Win } from "./ui/game/win/win.component.js";
+import {Game} from "./ui/game/game.component.js";
+import {data, subscribe} from "./data.js";
+import {Win} from "./ui/game/win/win.component.js";
 
 const appElement = document.querySelector('#app');
 
 function renderGame() {
-  appElement.innerHTML = '';
-  if (data.win) {
-    const win = Win();
-    appElement.append(win);
-  } else {
-    const game = Game();
-    appElement.append(game);
-  }
-
+    appElement.innerHTML = '';
+    if (data.win) {
+        const win = Win();
+        appElement.append(win);
+    } else {
+        const game = Game();
+        appElement.append(game);
+    }
 }
 
 renderGame()
